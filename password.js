b58ac8cc179f22a0af8077b05e56c2e220fc2461
Taoufik Ammi <lbladma@gmail.com>
2021-06-23 04:47:42 -0400

    JS file updated

--- password.js
+++ password.js
@@ -5,10 +5,72 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-
 }
 
-// Add event listener to generate button
+// Event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// Function to generate random password based on the user's selection. 
+function generatePassword() {
+  // The characters that are randomly used in the password
+  const upperCaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const lowerCaseLetters = "abcdefghijklmnopqrstuvwxyz";
+  const numbers = "0123456789";
+  const symbols = "!@#$%^&*_-+=";
+
+  // The initiated variables based on the user selection. 
+  let includeUpperCaseLetters;
+  let includeLowerCaseLetters;
+  let includeNumbers;
+  let includeSymbols;
+
+  // Prompting the user to enter the length of the characters. 
+  let length = parseInt(
+    prompt(
+      "Please enter the number of characters you'd like to have in your password (8 to 128 characters)."
+    )
+  );
+
+  // Prompt the user to try again if the correct number of characters are not met
+  if (!length) {
+    alert("Please click ok and try again. Your password must have at least 8 to 128 characters to proceed. ");
+  } else if (length < 8 || length > 128) {
+    // Verify the user has entered the correct number of characters as outlined. 
+    length = parseInt(prompt("You must choose between 8 and 128 characters."));
+  } else {
+    // Prompt the user to imput the characters as needed. 
+    includeUpperCaseLetters = confirm("Please Click ok to include uppercase letters in your password. Otherwise, click cancel. ");
+    includeLowerCaseLetters = confirm("Please Click ok to include lowercase letters in your password. Otherwise, click cancel. ");
+    includeNumbers = confirm(" Please Click ok to include numbers in your password. Otherwise, click cancel. ");
+    includeSymbols = confirm("Please Click ok to include symbols in your password. Otherwise, click cancel. ");
+  }
+
+  // Verify the user has entered the requested characters.  
+  if (
+    !includeUpperCaseLetters &&
+    !includeLowerCaseLetters &&
+    !includeNumbers &&
+    !includeSymbols
+  ) {
+    // Alert the user to select a character to proceed.
+    alert("You have not selected a character for your password. You must select a character to proceed. Please try again.");
+  }
+
+  // adding the character stings 
+  let characters = "";
+  includeUpperCaseLetters ? (characters += upperCaseLetters) : "";
+  includeLowerCaseLetters ? (characters += lowerCaseLetters) : "";
+  includeNumbers ? (characters += numbers) : "";
+  includeSymbols ? (characters += symbols) : "";
+
+  // Generate the password and convert it from an array toa string
+  let password = "";
+  for (let i = 0; i < length; i++) {
+    password += characters.charAt(
+      Math.floor(Math.random() * characters.length)
+    );
+  }
+  //Password provided to the user. 
+  return password;
+}
